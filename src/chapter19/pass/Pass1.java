0972604f218958d43a672c5cdb9bcb11ab2f39c2
Tomoharu Kawakubo <tomo.kawakubo@gmail.com>
2023-05-28 16:11:02 +0900

    2023年05月28日授業分

--- src/chapter19/pass/Pass1.java
+++ src/chapter19/pass/Pass1.java
@@ -1,5 +1,8 @@
 package chapter19.pass;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 record Book(int code, String title, int price) {}
@@ -12,5 +15,25 @@ public class Pass1 {
     public static void main(String[] args) {
         Supplier<Student> s = () -> new Student();
         System.out.println(s.get());
-    }    
+
+        final String s2 = "Java";
+        Predicate<String> p = s1 -> s1.contains(s2);
+        System.out.println(p.test("JavaScript"));
+
+        final String s3 = "JAVA";
+        p = s1 -> s1 != null && s1.equalsIgnoreCase(s3);
+        System.out.println(p.test("Java"));
+
+        var student = new Student(111, "横山花子");
+        Consumer<Student> m = st -> System.out.println(st.name());
+        m.accept(student);
+
+        printAboutBook(b -> b.price());
+        printAboutBook(b -> b.title().length());
+    }
+    
+    public static void printAboutBook(Function<Book, Integer> f) {
+        var book = new Book(1234, "簡単SpringBoot3", 2800);
+        System.out.println(f.apply(book));
+    }
 }
